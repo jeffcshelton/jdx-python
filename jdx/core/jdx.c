c6bb6ba41f78fc5ec5e71bf48abaf78dc0f01eba
jeffreycshelton <jeffrey.conner.shelton@gmail.com>
2021-11-26 13:51:01 -0500

    Write test source

---
+++ jdx/core/jdx.c
@@ -0,0 +1,63 @@
+#define PY_SSIZE_T_CLEAN
+
+#include <Python.h>
+#include "libjdx.h"
+
+typedef struct {
+	PyObject_HEAD
+} DatasetObject;
+
+static PyTypeObject DatasetType = {
+	PyObject_HEAD_INIT(NULL)
+		.tp_name = "jdx.Dataset",
+		.tp_doc = "Dataset object",
+		.tp_basicsize = sizeof(DatasetObject),
+		.tp_itemsize = 0,
+		.tp_flags = Py_TPFLAGS_DEFAULT,
+		.tp_new = PyType_GenericNew
+};
+
+static PyObject *read_dataset_from_path(PyObject *self, PyObject *args) {
+	const char *path;
+
+	if (!PyArg_ParseTuple(args, "s", &path)) {
+		return NULL;
+	}
+
+	JDXDataset dataset;
+	JDX_ReadDatasetFromPath(&dataset, path);
+	printf("Item count: %llu\n", dataset.header.item_count);
+
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
+static PyMethodDef jdxMethods[] = {
+	{ "read_dataset_from_path", read_dataset_from_path, METH_VARARGS, "Reads JDX Dataset from given path." },
+	{ NULL, NULL, 0, NULL }
+};
+
+static struct PyModuleDef jdxModule = {
+	PyModuleDef_HEAD_INIT,
+	"jdx",
+	"Python wrapper for libjdx.",
+	-1,
+	jdxMethods
+};
+
+PyMODINIT_FUNC PyInit_jdx(void) {
+	if (PyType_Ready(&DatasetType) < 0) return NULL;
+	Py_INCREF(&DatasetType);
+
+	PyObject *module = PyModule_Create(&jdxModule);
+	if (module == NULL) return NULL;
+
+	if (PyModule_AddObject(module, "Dataset", (PyObject *) &DatasetType) < 0) {
+		Py_DECREF(&DatasetType);
+		Py_DECREF(module);
+
+		return NULL;
+	}
+
+	return module;
+}
