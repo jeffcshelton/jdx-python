ffa225dcf393352728d98232b4b32705338e8996
Jeffrey Shelton <jeffrey.shelton.c@gmail.com>
2022-05-02 12:25:45 -0400

    Replace setup.cfg with setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+	with open("README.md") as file:
+		readme = file.read()
+
+	setup(
+		name="jdx",
+		version="0.4.0",
+		author="Jeffrey Shelton",
+		author_email="jeffrey.shelton.c@gmail.com",
+		description="Library for reading JDX files",
+		long_description=readme,
+		url="https://github.com/jeffreycshelton/jdx-python",
+		license="MIT",
+		packages=["jdx"],
+		install_requires=["numpy"]
+	)
