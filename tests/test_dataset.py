697470bb90a589c75702361059899da05050861e
Jeffrey Shelton <jeffrey.shelton.c@gmail.com>
2022-06-20 11:33:47 -0400

    Remove '../' from paths in tests

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -4,16 +4,16 @@ import os
 
 class TestDataset(unittest.TestCase):
 	def test_read(self):
-		Dataset.read_from("../res/example.jdx")
+		Dataset.read_from("res/example.jdx")
 	
 	def test_write(self):
-		example_dataset = Dataset.read_from("../res/example.jdx")
-		example_dataset.write_to("../res/temp.jdx")
+		example_dataset = Dataset.read_from("res/example.jdx")
+		example_dataset.write_to("res/temp.jdx")
 
-		read_dataset = Dataset.read_from("../res/temp.jdx")
+		read_dataset = Dataset.read_from("res/temp.jdx")
 		self.assertEqual(read_dataset, example_dataset)
 
-		os.remove("../res/temp.jdx")
+		os.remove("res/temp.jdx")
 
 if __name__ == "__main__":
 	unittest.main()
